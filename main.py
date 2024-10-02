08d7f57fe580a63096737e562fa1845b71134175
Harshali <135653287+harshaali1@users.noreply.github.com>
2024-10-02 20:13:49 +0530

    Add files via upload
    ~Race Game~

--- main.py
+++ main.py
@@ -1,48 +1,36 @@
-import turtle
 import time
 from turtle import Screen
-from snake import Snake
-from food import Food
-from scoreboard import Score
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
 screen = Screen()
-screen.setup(width=620, height=620)
-screen.title("Snake Game")
-screen.bgcolor("black")
+screen.setup(width=600, height=600)
 screen.tracer(0)
-
-snake = Snake()
-food = Food()
-score = Score()
+screen.bgcolor("black")
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+screen.onkey(player.go_up, "Up")
+
 
 game_is_on = True
-
 while game_is_on:
-    screen.update()
     time.sleep(0.1)
-    snake.move()
-
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        score.increase_score()
-
-    if snake.head.xcor() > 300 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -300:
-        score.reset()
-        snake.reset()
-
-    for segment in snake.segment[1:]:
-        if snake.head.distance(segment) < 10:
-            score.reset()
-            snake.reset()
-
-if game_is_on == False:
-    score.game_over()
+    screen.update()
     
+    car_manager.form_car()
+    car_manager.mov_cars()
+    
+    for car in car_manager.all_cars:
+        if car.distance(player) < 30:
+            game_is_on = False
+            scoreboard.gameover()
+    
+    if player.is_at_finish():
+            player.go_to_start()
+            car_manager.level_up()
+            scoreboard.increase_level()       
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,33 +1,26 @@
 from turtle import Turtle
+FONT = ("Courier", 24, "normal")
 
-ALIGNMENT = "center"
-FONT = ("Arial", 14, "normal")
 
-class Score(Turtle):
+
+
+class Scoreboard(Turtle):
     def __init__(self):
-        super().__init__()
-        self.score = 0
-        self.high_score = 0
-        self.penup()
-        self.hideturtle()
-        self.color("white")
-        self.goto(0, 280)
-        self.update_scoreboard()
-
-    def update_scoreboard(self):
+         super().__init__()
+         self.level = 1
+         self.hideturtle()
+         self.penup()
+         self.color("white")
+         self.goto(-280,250)
+         self.update_score()
+         
+    def update_score(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
     
-    #def game_over(self):
-     #   self.goto(0,0)
-      #  self.write("GAME OVER", align = "center", font = FONT)
-        
-    def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
-        self.score = 0
-        self.update_scoreboard()
-
-    def increase_score(self):
-        self.score += 1
-        self.update_scoreboard()
+    def inc_level(self):
+        self.level += 1
+        self.update_score()
+    def gameover(self) :        
+        self.goto(0,0)
+        self.write(f"KHATAMA HOGYA TERA", align="center", font=FONT)
